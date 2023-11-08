a7bf27cd8874974ce867f9fafff73e94c63a84c7
jccalhoun <jccalhoun@gmail.com>
2023-11-07 20:21:10 -0500

    updated background
    started implementing background color change for yellow color

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -17,6 +17,12 @@ let redBackground;
         clearInterval(Interval);
         Interval = setInterval(startTimer, 1000);
         document.body.style.backgroundColor = "green"; //this sets the background to green when the start button is clicked
+        if (timeLimit > 1) {
+        yellowBackground = (timeLimit - 1)*60;
+        } else {
+            yellowBackground = 30;
+        } //if timeLimit is more than a minute it sets yellow to a minute less than the limit. if not it sets it to 30 seconds. currently doesn't work because the seconds only counts up to 59 then resets to 0. 
+        console.log(yellowBackground);
     };
     //so this calls startTimer and every time startTimer is called it adds one to tens so I think i could do something like set a goal variable and when buttonStart sets the background to Green and time > 70% of goal or something it turns the background yellow
     buttonStop.onclick = function () {
@@ -30,12 +36,13 @@ let redBackground;
         minutes = "00";
         appendSeconds.innerHTML = seconds;
         appendMinutes.innerHTML = minutes;
+        document.body.style.backgroundColor = "#ffa600"; //sets the background back to the default orangish
     };
 
     function startTimer() {
         seconds++;
-        if (seconds > timeLimit) {
-            document.body.style.backgroundColor = "blue";
+        if (seconds > yellowBackground) {
+            document.body.style.backgroundColor = "yellow";
         }
         if (seconds <= 9) {
             appendSeconds.innerHTML = "0" + seconds;
@@ -58,4 +65,4 @@ let redBackground;
         }
     }
 };
-console.log(parseInt(document.getElementById("mySelect").value));
+//console.log(parseInt(document.getElementById("mySelect").value));
